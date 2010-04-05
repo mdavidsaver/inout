6a9994198d3f2834272733a9b104b7960b2f7d1a
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-04-05 14:20:28 -0400

    works with bzr fastimport 0.9

--- git-bzr-pull.sh
+++ git-bzr-pull.sh
@@ -16,20 +16,13 @@ export GIT_DIR="$PWD/.git"
 # Parse arguments
 
 remote="$1"
-rbnh="$2"
-lbnh="$3"
+lbnh="$2"
 
 [ -d "$remote" ] || die "bzr repository must be local"
 
-[ -z "$rbnh" ] && die "Must specify bazaar branch"
-
 if [ -z "$lbnh" ]
 then
-  if [ "$rbnh" = "root" ]; then
-    lbnh="master"
-  else
-    lbnh="$rbnh"
-  fi
+  lbnh="master"
 fi
 
 # Setup/checkout marks tracking branch
@@ -42,7 +35,7 @@ then
 
   gargs="--import-marks=$BASE/marks.git"
   bargs="--import-marks=$BASE/marks.bzr"
-elif git branch --no-color|grep "^origin/inout$" &>/dev/null
+elif git branch -r --no-color|grep "^[ \*] origin/inout$" &>/dev/null
 then
   # yes (from clone)
   git checkout -b "inout" "origin/inout" || die "Failed to checkout inout/$lbnh"
@@ -62,23 +55,36 @@ fi
 # rbnh   - the bzr branch to be imported
 # lbnh   - the git name of the imported branch
 
-cho "Pull $rbnh as $lbnh from $remote"
+echo "Pull as $lbnh from $remote"
 
 cat << EOF > .msg
-$(basename $0) pull from $rbnh as $lbnh
+$(basename $0) pull from $remote
 
 remote $remote
 EOF
 
 pushd "$remote" &>/dev/null || die "Failed to cd to $remote"
 
-bzr fast-export --plain -b $lbnh --marks=$BASE/marks.bzr $rbnh | \
+bzr fast-export --plain -b $lbnh --marks=$BASE/marks.bzr . | \
 git fast-import $gargs --export-marks=$BASE/tmp.git \
 || echo ">>>>>> Pull Error <<<<<<"
 
 popd &>/dev/null
 
 mv tmp.git marks.git || echo "Missing tmp.git???"
+
+if head -n1 marks.bzr|grep '^format' &>/dev/null
+then
+  # ok
+  echo -n
+else
+  # the header is backwards!
+  head -n1 marks.bzr > h1 || die "Failed to extract header 1"
+  head -n2 marks.bzr | tail -n1 > h2 || die "Failed to extract header 1"
+  tail -n '+1' marks.bzr > b || die "Failed to extract body"
+  cat h2 h1 b > marks.bzr || die "Failed to rebuild marks.bzr"
+  rm -f h1 h2 b
+fi
 
 sort -g marks.bzr > tmp.bzr || die "Failed to sort bzr output"
 mv tmp.bzr marks.bzr || die "???"
